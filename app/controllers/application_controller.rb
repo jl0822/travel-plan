56c6893208e163dbefa94a2e4795462355fdf2fb
jeff <jluicanada@gmail.com>
2020-04-15 14:18:43 +0900

    Found and fixed syntax error in route.rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ module Api
         private
         
         def authenticate_request
-        @current_api_user = AuthorizeApiRequest.call(request.headers)
-                                                .result
+        @current_api_user = AuthorizeApiRequest.call(request.headers).result
+                                                
         
         render json: {  error: "This is not a authorized request." }, 
                         status: :unauthorized unless @current_api_user
